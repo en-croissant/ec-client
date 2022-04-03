0a0c5b68516128481fa98b7de3d6c67d376064ed
rwclutterbuck <rwclutterbuck@outlook.com>
2022-04-03 18:58:13 +0100

    Add: basic chess board

--- src/App.js
+++ src/App.js
@@ -2,30 +2,48 @@ import logo from "./logo.svg";
 import "./App.css";
 import { io } from "socket.io-client";
 
+import { useState } from "react";
+import Chess from "chess.js";
+import { Chessboard } from "react-chessboard";
+
 const socket = io("https://en-croissant.herokuapp.com/");
 
 function App() {
-  socket.on("hello world", () => {
-    console.log("hello world");
-  });
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [game, setGame] = useState(new Chess());
+
+  function safeGameMutate(modify) {
+    setGame((g) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
+  }
+
+  function makeRandomMove() {
+    const possibleMoves = game.moves();
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
+      return; // exit if the game is over
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    safeGameMutate((game) => {
+      game.move(possibleMoves[randomIndex]);
+    });
+  }
+
+  function onDrop(sourceSquare, targetSquare) {
+    let move = null;
+    safeGameMutate((game) => {
+      move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q", // always promote to a queen for example simplicity
+      });
+    });
+    if (move === null) return false; // illegal move
+    setTimeout(makeRandomMove, 200);
+    return true;
+  }
+
+  return <Chessboard position={game.fen()} onPieceDrop={onDrop} />;
 }
 
 export default App;
