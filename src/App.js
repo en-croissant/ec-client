9ad97e2322d65f1310853a386fd48489e856e040
jalexxx <jumaanahalex@gmail.com>
2022-04-03 23:22:21 +0100

    made a temp header layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Routes, Route } from "react-router-dom";
 import * as Pages from "./pages";
 import { useAuthContext } from "./contexts/auth";
+import { Header } from "./layout";
 
 import "./App.css";
 
@@ -17,6 +18,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path={"/"} element={<Pages.Home />} />
       </Routes>
