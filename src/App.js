8c36bc3e53cefeb5645eb9482df14805ed9f5232
jalexxx <jumaanahalex@gmail.com>
2022-04-03 22:51:04 +0100

    commented out switch and made an index.css for homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from "./logo.svg";
 import { Home } from "./pages";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { io } from "socket.io-client";
 
 const socket = io("https://en-croissant.herokuapp.com/");
@@ -12,11 +12,11 @@ function App() {
   });
   return (
     <>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
+      {/* <Switch> */}
+      <Route exact path="/">
+        <Home />
+      </Route>
+      {/* </Switch> */}
     </>
   );
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,7 +3,8 @@ import React from "react";
 function Home() {
   <>
     <h1> hello </h1>
-  </>;
+  </>
+  
 }
 
 export default Home;
