04533e2e7d16f8ead25659608285bb5529587ecb
jalexxx <jumaanahalex@gmail.com>
2022-04-03 23:23:37 +0100

    added a return to home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./style.css";
 
 function Home() {
-  <>
-    <div id="titlediv">
-      <h1> hello </h1>
-    </div>
-  </>;
+  return (
+    <>
+      <div id="titlediv">
+        <h1> hello </h1>
+      </div>
+    </>
+  );
 }
 
 export default Home;
