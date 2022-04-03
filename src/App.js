48fcdadb56b2c66f51502dbe849177e13c7c4755
jalexxx <jumaanahalex@gmail.com>
2022-04-03 18:02:10 +0100

    added react-router-dom to dependecies oe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from "./logo.svg";
 import { Home } from "./pages";
 import "./App.css";
+
 import { io } from "socket.io-client";
 
 const socket = io("https://en-croissant.herokuapp.com/");
