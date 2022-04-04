edf9c71948fb2a7426fd2d007669b651c8200cf5
rwclutterbuck <rwclutterbuck@outlook.com>
2022-04-04 10:20:04 +0100

    Move: chessboard to component

--- src/App.js
+++ src/App.js
@@ -2,77 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { io } from "socket.io-client";
 
-import { useRef, useState } from "react";
-import Chess from "chess.js";
-import { Chessboard } from "react-chessboard";
+import Gameboard from "./Chessboard.js";
 
-//const socket = io("https://en-croissant.herokuapp.com/");
-const socket = io("localhost:5000");
+const socket = io("https://en-croissant.herokuapp.com/");
+//const socket = io("localhost:5000");
 
 function App() {
   socket.on("hello world", ({ data }) => console.log(data));
 
-  const chessboardRef = useRef();
-  const [game, setGame] = useState(new Chess());
-
-  function safeGameMutate(modify) {
-    setGame((g) => {
-      const update = { ...g };
-      modify(update);
-      return update;
-    });
-  }
-
-  function onDrop(sourceSquare, targetSquare) {
-    const gameCopy = { ...game };
-    const move = gameCopy.move({
-      from: sourceSquare,
-      to: targetSquare,
-      promotion: "q", // always promote to a queen for example simplicity
-    });
-    console.log(move.san);
-    socket.emit("move", move.san);
-    setGame(gameCopy);
-    return move;
-  }
-
   return (
-    <div>
-      <Chessboard
-        id="PlayVsPlay"
-        animationDuration={200}
-        boardWidth={400}
-        position={game.fen()}
-        onPieceDrop={onDrop}
-        customBoardStyle={{
-          borderRadius: "4px",
-          boxShadow: "0 5px 15px rgba(0, 0, 0, 0.5)",
-        }}
-        ref={chessboardRef}
-      />
-      <button
-        className="rc-button"
-        onClick={() => {
-          safeGameMutate((game) => {
-            game.reset();
-          });
-          chessboardRef.current.clearPremoves();
-        }}
-      >
-        reset
-      </button>
-      <button
-        className="rc-button"
-        onClick={() => {
-          safeGameMutate((game) => {
-            game.undo();
-          });
-          chessboardRef.current.clearPremoves();
-        }}
-      >
-        undo
-      </button>
-    </div>
+    <>
+      <Gameboard socket={socket} />
+    </>
   );
 }
 
