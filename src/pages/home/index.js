b7769fc6d11c3d29cf70aba25bfe7f0c845c53a8
jalexxx <jumaanahalex@gmail.com>
2022-04-04 12:39:29 +0100

    made black diagonal shape for black left div

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,7 +5,9 @@ function Home() {
   return (
     <>
       <div id="homeDiv">
-        <div id="black_l"></div>
+        <div id="black_l">
+          <div id="black_dgnl"></div>
+        </div>
         <div id="white_l">
           <div id="white_dgnl"></div>
         </div>
