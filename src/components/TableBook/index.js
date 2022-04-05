339febc718b1282f177bb1708cbd7abc158e5063
jalexxx <jumaanahalex@gmail.com>
2022-04-05 10:12:06 +0100

    made an a href tag for entire book to be a button

--- src/components/TableBook/index.js
+++ src/components/TableBook/index.js
@@ -3,9 +3,11 @@ import React from "react";
 const TableBook = () => {
   return (
     <>
-      <div id="top_book"></div>
-      <div id="side_book"></div>
-      <div id="pages"></div>
+      <a href="/Book">
+        <div id="top_book"></div>
+        <div id="side_book"></div>
+        <div id="pages"></div>
+      </a>
     </>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as JoinGame } from "./JoinGame";
-export { default as GameChessboard} from "./GameChessboard";
+export { default as GameChessboard } from "./GameChessboard";
+export { default as TableBook } from "./TableBook";
