55fdd393328c5b1da9300ce88c282e14dcc128bd
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-04-05 17:09:02 +0100

    join game button redirects to /play

--- src/components/JoinGame/index.js
+++ src/components/JoinGame/index.js
@@ -1,5 +1,8 @@
 import React from "react"; 
-import { useState } from "react";
+import { useState, useEffect} from "react";
+import { io } from "socket.io-client"
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
 const JoinGame = () => {
   
@@ -10,9 +13,30 @@ const JoinGame = () => {
   const [gameMode, setGameMode] = useState("");
   const [timeLimit, setTimeLimit] = useState("");
   const [saveGame, setSaveGame] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const socket = io("https://en-croissant.herokuapp.com");
+
+  const settings = { difficultyAI, gameMode, timeLimit, saveGame }
+
+  const fetchGameLobby = async () => {
+    const { settings } = await axios.post("https://en-croissant.herokuapp.com/lobby/");
+    set(settings);
+  };
+
+  useEffect(fetchGameLobby, []);
+
+
+  const onClickEvent = (e) => {
+    e.preventDefault();
+    setRedirect(true);
+  };
 
     return (
       <>
+
+        {redirect ? <Redirect to="/play" /> : undefined}
+
         <div>
           <h2>Game settings</h2>
           <ul>
@@ -25,6 +49,10 @@ const JoinGame = () => {
             <li>Save game: {saveGame}</li>
           </ul>
         </div>
+
+        <button
+        onClick={(e)=> onClickEvent(e)}
+        >Play Game</button>
       </>
     );
   };
