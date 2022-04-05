2760b6bdbe44a0a4551955fdad7801c31fec6613
jalexxx <jumaanahalex@gmail.com>
2022-04-05 12:38:52 +0100

    deleted the chess board and uncommented previous divs

--- src/components/TableObjects/index.js
+++ src/components/TableObjects/index.js
@@ -11,20 +11,10 @@ const TableObjects = () => {
         <div id="pages"></div>
       </a>
 
-      <div id="chessboard">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        
-      </div>
-      {/* <div id="chess_base"></div>
-      <div id="chess_base_inner"></div> */}
-      {/* <div id="chess_left"></div>
-      <div id="chess_front"></div> */}
+      <div id="chess_base"></div>
+      <div id="chess_base_inner"></div>
+      <div id="chess_left"></div>
+      <div id="chess_front"></div>
     </>
   );
 };
