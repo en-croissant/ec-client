53798efb95b9adec334bede93ed7e417f335fa6e
Noah Platton <noahplatton@gmail.com>
2022-04-05 10:44:39 +0100

    export login and registration forms

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as JoinGame } from "./JoinGame";
 export { default as GameChessboard} from "./GameChessboard";
+export { default as LoginForm } from './LoginForm'
+export { default as RegForm } from './RegForm'
