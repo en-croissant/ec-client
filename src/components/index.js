3f2b2bf310098dd0fbd2a59734228279a2e84f6f
jalexxx <jumaanahalex@gmail.com>
2022-04-05 11:53:23 +0100

    changed TableBook to TableObjects

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as JoinGame } from "./JoinGame";
-export { default as LoginForm } from './LoginForm'
-export { default as RegForm } from './RegForm'
+export { default as LoginForm } from "./LoginForm";
+export { default as RegForm } from "./RegForm";
 export { default as GameChessboard } from "./GameChessboard";
-export { default as TableBook } from "./TableBook";
+export { default as TableObjects } from "./TableObjects";
