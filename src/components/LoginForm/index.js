ff128b453ed148130a58368519815f7159777eec
jalexxx <jumaanahalex@gmail.com>
2022-04-06 10:31:15 +0100

    made the back of clipboard

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -44,54 +44,56 @@ function LoginForm() {
 
   return (
     <>
-      {/* <div id="clipboard_back"></div> */}
-      <form aria-label="login-form" id="login_form" onSubmit={handleSubmit}>
-        <div id="back_paper"></div>
-        <div>
-          <h1> Login to compete again</h1>
-          <div id="user_icon"></div>
-          <label htmlFor="username">Username:</label>
-          <input
-            className="password-field"
-            aria-label="username-field"
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleInput}
-            placeholder="Enter username"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            className="cb pristine"
-            aria-label="password-field"
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInput}
-            placeholder="Password"
-            required
-          />
-        </div>
-
-        <div id="btn_doodle">
-          <input
-            id="submit_btn"
-            role="button"
-            type="submit"
-            disabled={formIncomplete()}
-            value="Login"
-          />
-          <svg viewBox="0 0 500 150" preserveAspectRatio="none">
-            <path
-              fill="none"
-              d="M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7"
+      <div id="clipboard_back">
+        <form aria-label="login-form" id="login_form" onSubmit={handleSubmit}>
+          <div id="back_paper"></div>
+          <div>
+            <h1> Login to compete again</h1>
+            <div id="user_icon"></div>
+            <label htmlFor="username">Username:</label>
+            <input
+              id="cb pristine"
+              className="password-field"
+              aria-label="username-field"
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleInput}
+              placeholder="Enter username"
+              required
             />
-          </svg>
-        </div>
-      </form>
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              id="cb pristine"
+              aria-label="password-field"
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInput}
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          <div id="btn_doodle">
+            <input
+              id="submit_btn"
+              role="button"
+              type="submit"
+              disabled={formIncomplete()}
+              value="Login"
+            />
+            <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+              <path
+                fill="none"
+                d="M325,18C228.7-8.3,118.5,8.3,78,21C22.4,38.4,4.6,54.6,5.6,77.6c1.4,32.4,52.2,54,142.6,63.7 c66.2,7.1,212.2,7.5,273.5-8.3c64.4-16.6,104.3-57.6,33.8-98.2C386.7-4.9,179.4-1.4,126.3,20.7"
+              />
+            </svg>
+          </div>
+        </form>
+      </div>
       {error && (
         <div data-testid="error" id="error">
           {error}
