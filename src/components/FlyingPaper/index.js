41eb368e0cc7b16b94351170d407d712e4f3dcb7
jalexxx <jumaanahalex@gmail.com>
2022-04-06 18:24:58 +0100

    made a component

---
+++ src/components/FlyingPaper/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./style.css";
+
+function FlyingPaper() {
+  return (
+    <>
+      {/* <input type="checkbox" id="light-switch" />
+      <label for="light-switch" id="light-switch-label">
+        <div className="screw"></div>
+        <div className="switch"></div>
+        <div className="screw"></div>
+      </label> */}
+    </>
+  );
+}
+
+export default FlyingPaper;
