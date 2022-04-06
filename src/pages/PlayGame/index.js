e0185f886fcba2fb25a1e096a22c0a0a95e86207
Noah Platton <noahplatton@gmail.com>
2022-04-06 11:56:38 +0100

    style: DELETED CODE

--- src/pages/PlayGame/index.js
+++ src/pages/PlayGame/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import {GameChessboard } from "../../components";
 import { io } from "socket.io-client"
 
+const socket = io("https://en-croissant.herokuapp.com");
+
 function PlayGame() {
 
-
-    const socket = io("https://en-croissant.herokuapp.com");
     socket.on("hello world", ({ data }) => console.log(data));
+
 
     return (
         <>
@@ -19,10 +20,5 @@ function PlayGame() {
 
 export default PlayGame;
 
-// getting session id and  user id (for both from the JWT) that is all the info we need to start a game
-// get that and have it ready to send to the sockets, rich can help connecting it to the sockets
 
-// 
-// 
-// dont want make random move but have savegamemutate from github 
 
