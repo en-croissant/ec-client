b6088bdc56b274467d03839878a454e04b0ae9a4
Noah Platton <noahplatton@gmail.com>
2022-04-06 14:35:03 +0100

    test(App.js): skip irrelevant test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import * as Pages from "./pages";
 
-test("renders learn react link", () => {
+xtest("renders learn react link", () => {
   render(<Pages.Home />);
   expect(screen).toBeDefined();
 });
