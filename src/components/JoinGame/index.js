8f0dd07d951c4e9064a70bcb06c2bcf37acb135d
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-04-06 17:51:24 +0100

    adding before a merger

--- src/components/JoinGame/index.js
+++ src/components/JoinGame/index.js
@@ -10,20 +10,20 @@ const JoinGame = () => {
   const [username1, setUsername1] = useState("");
   const [username2, setUsername2] = useState("");
   const [roomName, setRoomName] = useState("");
-  const [difficultyAI, setDifficultyAI] = useState("");
-  const [gameMode, setGameMode] = useState("");
-  const [timeLimit, setTimeLimit] = useState("");
-  const [saveGame, setSaveGame] = useState("");
+  // const [difficultyAI, setDifficultyAI] = useState("");
+  // const [gameMode, setGameMode] = useState("");
+  // const [timeLimit, setTimeLimit] = useState("");
+  // const [saveGame, setSaveGame] = useState("");
   const [redirect, setRedirect] = useState(false);
 
   const socket = io("https://en-croissant.herokuapp.com");
 
-  const settings = { difficultyAI, gameMode, timeLimit, saveGame }
-  const navigate = useNavigate;
+  const settings = { username1, username2, lobby_id  }
+  const navigate = useNavigate();
 
   const fetchGameLobby = async () => {
-    const { settings } = await axios.post("https://en-croissant.herokuapp.com/lobby/");
-    // set(settings);
+    const { data } = await axios.get(`https://en-croissant.herokuapp.com/lobby/${lobby_id}`);
+    // set(data);
   };
 
   useEffect(fetchGameLobby, []);
@@ -43,12 +43,12 @@ const JoinGame = () => {
           <h2>Game settings</h2>
           <ul>
             <li>Room name: {roomName}</li>
-            <li>Player 1: {username1}</li>
+            <li>Player 1 (host): {username1}</li>
             <li>Player 2: {username2}</li>
-            <li>AI difficulty: {difficultyAI}</li>
+            {/* <li>AI difficulty: {difficultyAI}</li>
             <li>Game mode: {gameMode}</li>
             <li>Time limit: {timeLimit}</li>
-            <li>Save game: {saveGame}</li>
+            <li>Save game: {saveGame}</li> */}
           </ul>
         </div>
 
