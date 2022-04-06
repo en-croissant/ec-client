217aa4687ee8f3d90facded8a20e2263c47c355b
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-04-06 18:15:03 +0100

    merger pt2

--- src/components/JoinGame/index.js
+++ src/components/JoinGame/index.js
@@ -18,17 +18,6 @@ const JoinGame = () => {
 
   const socket = io("https://en-croissant.herokuapp.com");
 
-<<<<<<< HEAD
-  const settings = { username1, username2, lobby_id  }
-  const navigate = useNavigate();
-
-  const fetchGameLobby = async () => {
-    const { data } = await axios.get(`https://en-croissant.herokuapp.com/lobby/${lobby_id}`);
-    // set(data);
-  };
-
-  useEffect(fetchGameLobby, []);
-=======
   // const settings = { difficultyAI, gameMode, timeLimit, saveGame }
   
   useEffect(() => {
@@ -44,7 +33,6 @@ const JoinGame = () => {
     }
     fetchLobbyData()
   }, []);
->>>>>>> bc5250f9747cdfdf996997552a416187680f4799
 
 
   const onClickEvent = (e) => {
@@ -57,20 +45,6 @@ const JoinGame = () => {
 
       {redirect ? navigate("/play") : undefined}
 
-<<<<<<< HEAD
-        <div>
-          <h2>Game settings</h2>
-          <ul>
-            <li>Room name: {roomName}</li>
-            <li>Player 1 (host): {username1}</li>
-            <li>Player 2: {username2}</li>
-            {/* <li>AI difficulty: {difficultyAI}</li>
-            <li>Game mode: {gameMode}</li>
-            <li>Time limit: {timeLimit}</li>
-            <li>Save game: {saveGame}</li> */}
-          </ul>
-        </div>
-=======
       <div>
         <h2>Game settings</h2>
         <ul>
@@ -83,7 +57,6 @@ const JoinGame = () => {
           <li>Save game: {saveGame}</li>
         </ul>
       </div>
->>>>>>> bc5250f9747cdfdf996997552a416187680f4799
 
       <button
       onClick={(e)=> onClickEvent(e)}
