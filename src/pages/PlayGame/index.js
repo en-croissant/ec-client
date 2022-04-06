a8de299a53cfdb7bc68f1994f2cdbee3482622a2
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-04-06 11:29:55 +0100

    comments for sockets added

--- src/pages/PlayGame/index.js
+++ src/pages/PlayGame/index.js
@@ -20,3 +20,7 @@ export default PlayGame;
 
 // getting session id and  user id (for both from the JWT) that is all the info we need to start a game
 // get that and have it ready to send to the sockets, rich can help connecting it to the sockets
+
+// 
+// 
+// dont want make random move but have savegamemutate from github 
