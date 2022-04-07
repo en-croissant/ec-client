42d80fc381a67cdb68da6ae55ebe79af4c13f09f
Noah Platton <noahplatton@gmail.com>
2022-04-07 17:51:40 +0100

    commit to merge

--- src/components/GameChessboard/index.test.js
+++ src/components/GameChessboard/index.test.js
@@ -1,7 +1,18 @@
 import { default as GameChessboard } from ".";
 import { screen, render } from "@testing-library/react";
 
+import * as Stuff from 'react'
+
 describe("GameChessboard", () => {
-  xit("successfully makes socket connections", () => {
+  it("successfully renders the chessboard", () => {
+    const mockSetState = jest.fn()
+    const mockUseState = jest.fn(() => ["tester", mockSetState])
+    const mockValues = {
+      setState: mockUseState
+    }
+    jest.spyOn(Stuff, 'useState').mockImplementation(() => mockValues)
+    renderWithProviders(<GameChessboard/>)
+    const chessboard = screen.getByLabelText('chessboard')
+    expect(chessboard).toBeInTheDocument()
   });
 });
