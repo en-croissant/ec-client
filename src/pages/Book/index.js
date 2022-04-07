e9931624023f0c2079d668667a6e6a7463dacf77
saminakhan999 <samina.khan9999@gmail.com>
2022-04-07 04:03:22 +0100

    added go back to book page

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -8,18 +8,25 @@ function Book(props) {
   return (
     <>
       <div>
-        <h1 >Hello</h1>
-      </div>
-      <HTMLFlipBook width={300} height={500}>
+        <div>
+          <h1>Hello</h1>
+        </div>
+        <HTMLFlipBook className="HTMLFlipBook" width={300} height={500}>
           <div className="nopage"></div>
-          <div className="bookfront"><div className="bookfronttext">NOTEBOOK</div></div>
+          <div className="bookfront">
+            <div className="bookfronttext">NOTEBOOK</div>
+          </div>
           <div className="leftpage">Page 1</div>
           <div className="rightpage">Page 2</div>
           <div className="leftpage">Page 3</div>
           <div className="rightpage">Page 4</div>
           <div className="leftpage">Page 5</div>
           <div className="rightpage">Page 6</div>
-      </HTMLFlipBook>
+        </HTMLFlipBook>
+      </div>
+      <a className="goBack" href="/">
+        <span>Go back</span>
+      </a>
     </>
   );
 }
