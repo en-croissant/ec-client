840ed2776fd2d4c7f00b29a2d3b7437cbd4ca917
Noah Platton <noahplatton@gmail.com>
2022-04-07 09:23:15 +0100

    test(Header): wrap statements in act

--- src/layout/Header/Header.test.js
+++ src/layout/Header/Header.test.js
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { screen, act } from '@testing-library/react'
 
 import * as AuthContext from '../../contexts/auth'
 
@@ -11,14 +11,16 @@ describe("Header", () => {
   //   expect(logout).not.toBeInTheDocument()
   // })
 
-  test("on button click the logout function gets called", () => {
+  test("on button click the logout function gets called", async () => {
     const mockLogout = jest.fn()
     const mockValues = {
       logout: mockLogout
     }
     jest.spyOn(AuthContext, 'useAuthContext').mockImplementation(() => mockValues)
-    renderWithProviders(<Header/>)
-    userEvent.click(screen.getByRole('button'))
+    await act(async () => {
+      renderWithProviders(<Header/>)
+      userEvent.click(screen.getByRole('button'))
+    })
     expect(mockLogout).toHaveBeenCalled()
   })
 })
