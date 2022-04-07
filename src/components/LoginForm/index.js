8f77fb694a83ffcd4cd1721f7ffedefee076c8d8
Noah Platton <noahplatton@gmail.com>
2022-04-07 23:41:56 +0100

    style: fx spacing of inputs on login form

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -49,7 +49,7 @@ function LoginForm() {
         <form aria-label="login-form" id="login_form" onSubmit={handleSubmit}>
           <div id="back_paper"></div>
           <div>
-            <h1> Login Sheet</h1>
+            <h1 id="login-title"> Login Sheet</h1>
             <div id="user_icon"></div>
             <div className="field-div">
             <label htmlFor="username">Username:</label>
