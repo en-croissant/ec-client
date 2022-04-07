4437108561292322f110a40b3c13a69d0030dc5f
jalexxx <jumaanahalex@gmail.com>
2022-04-07 14:33:33 +0100

    putting clipboard and paper behind join game

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -7,18 +7,16 @@ function Main() {
   return (
     <>
       <Header />
-      <div id="cork_board"></div>
-      <div id="poster">
-        <div id="poster_border">
-          <div id="inner_border">
-            <h1> This is the page after you log in</h1>
-            <a href="/create">
-              <div id="createGame">Create Game</div>
-            </a>
-            <a href="/lobby">
-              <div id="joinGame">Join Game</div>
-            </a>
-          </div>
+      <div id="main_clipboard_back">
+        <div id="main_clip"></div>
+        <div id="main_back_paper">
+          <h1> This is the page after you log in</h1>
+          <a href="/create">
+            <div id="createGame">Create Game</div>
+          </a>
+          <a href="/lobby">
+            <div id="joinGame">Join Game</div>
+          </a>
         </div>
       </div>
     </>
