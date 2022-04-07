d14330dbe6f7677be6ab8fec65ea78895eb66359
saminakhan999 <samina.khan9999@gmail.com>
2022-04-07 05:49:35 +0100

    created header after login

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,16 +1,23 @@
 import React from "react";
-import "./style.css";
-
+import { useAuthContext } from "../../contexts/auth";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const { logout } = useAuthContext();
+  const navigate = useNavigate();
 
+  const handleLogout = async () => {
+    await logout();
+    navigate("/");
+  };
 
   return (
     <>
+      <span className = "header"> 
+        <input type="submit" onClick={handleLogout} value="Logout" />
+      </span>
     </>
   );
 }
 
 export default Header;
-
-
