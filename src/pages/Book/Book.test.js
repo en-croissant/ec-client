d9c23e4b24752c2c0aed260be9bd7f0eb0182ff7
Noah Platton <noahplatton@gmail.com>
2022-04-07 09:30:23 +0100

    test: add test to Book page

---
+++ src/pages/Book/Book.test.js
@@ -0,0 +1,11 @@
+import { screen } from '@testing-library/react'
+
+import { default as Book } from '.'
+
+describe("Book page", () => {
+  test("it renders the flip book", () => {
+    renderWithProviders(<Book/>)
+    const flipbook = screen.getByText(/notebook/i)
+    expect(flipbook).toBeInTheDocument()
+  })
+})
