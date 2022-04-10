0530579240286acb0e6976485f87e2d00504cb84
Noah Platton <noahplatton@gmail.com>
2022-04-10 11:49:47 +0100

    test(Lobby): remove unneeded test

--- src/pages/Lobby/Lobby.test.js
+++ src/pages/Lobby/Lobby.test.js
@@ -9,10 +9,4 @@ describe("Lobby page", () => {
     expect(lobbyTitle).toBeInTheDocument()
     expect(lobbyTitle.textContent).toMatch(/join now/i)
   })
-
-  test("it renders a button to return to home", () => {
-    renderWithProviders(<Lobby/>)
-    const goBack = screen.getByRole('link')
-    expect(goBack).toHaveAttribute('href', '/')
-  })
 })
